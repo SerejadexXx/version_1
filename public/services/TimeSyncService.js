cf4f00dd93e9dcea6ec960217e7f170cb31ee772
Сергей Нагин Sergey Nagin <Sereja@Sergejs-MacBook-Pro.local>
2016-02-05 03:10:05 +0200

    new commit

--- public/services/TimeSyncService.js
+++ public/services/TimeSyncService.js
@@ -65,6 +65,7 @@ module.service('TimeSyncFunctional', function($http, $timeout) {
     };
 
     var _AddInterval = function(interval) {
+        console.log(interval);
         _intervals.push(interval);
         while (_intervals.length > 10 && !_CheckIntervals()) {
             _intervals.splice(0, 1);
